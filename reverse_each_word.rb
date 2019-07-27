084c83b8cfddeb8ef210a795904572a507bcdec0
Julie Cook <juliecook328@gmail.com>
2019-07-27 18:26:28 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,7 @@ def reverse_each_word(string)
   a = string.split
   a.collect  {|word| word.reverse}.join(' ')
 end 
+
  # { |b| "#{b.reverse} on #{"bread".reverse}" }
 # oppressed_workers = ["Dopey", "Sneezy", "Happy", "Angry", #"Doc", "Lemonjello", "Sleepy" ]
 #oppressed_workers.each do |oppressed_worker|
