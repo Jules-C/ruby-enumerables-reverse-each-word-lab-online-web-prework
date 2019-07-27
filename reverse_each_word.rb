b4fd830d0ff9b65d14d38544929a7e812ae1d517
Julie Cook <juliecook328@gmail.com>
2019-07-27 18:12:10 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,7 +4,6 @@ def reverse_each_word(string)
    b = a.reverse 
    b.join(' ')
   end
-  
 end 
  # { |b| "#{b.reverse} on #{"bread".reverse}" }
 # oppressed_workers = ["Dopey", "Sneezy", "Happy", "Angry", #"Doc", "Lemonjello", "Sleepy" ]
